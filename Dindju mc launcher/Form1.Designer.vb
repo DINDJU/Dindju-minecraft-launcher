c64fb1e94439ad04c82a3d59ed4c31cf2c08a57b
DINDJU <mathisdzme@outlook.fr>
2025-07-02 11:38:53 +0200

    Add files via upload

---
+++ Dindju mc launcher/Form1.Designer.vb
@@ -0,0 +1,105 @@
+﻿
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtUsername = New System.Windows.Forms.TextBox()
+        Me.btnLaunch = New System.Windows.Forms.Button()
+        Me.cmbVersion = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.txtUsername.Location = New System.Drawing.Point(15, 31)
+        Me.txtUsername.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(215, 26)
+        Me.txtUsername.TabIndex = 0
+        '
+        'btnLaunch
+        '
+        Me.btnLaunch.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnLaunch.Location = New System.Drawing.Point(15, 117)
+        Me.btnLaunch.Margin = New System.Windows.Forms.Padding(4)
+        Me.btnLaunch.Name = "btnLaunch"
+        Me.btnLaunch.Size = New System.Drawing.Size(215, 32)
+        Me.btnLaunch.TabIndex = 1
+        Me.btnLaunch.Text = "Jouer"
+        Me.btnLaunch.UseVisualStyleBackColor = True
+        '
+        'cmbVersion
+        '
+        Me.cmbVersion.FormattingEnabled = True
+        Me.cmbVersion.Location = New System.Drawing.Point(15, 83)
+        Me.cmbVersion.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmbVersion.Name = "cmbVersion"
+        Me.cmbVersion.Size = New System.Drawing.Size(215, 26)
+        Me.cmbVersion.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(50, 18)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "pseudo"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 18)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Version"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(243, 162)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbVersion)
+        Me.Controls.Add(Me.btnLaunch)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Dindju launcher"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents btnLaunch As Button
+    Friend WithEvents cmbVersion As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Dindju mc launcher/Form1.vb
@@ -0,0 +1,152 @@
+﻿Imports System
+Imports System.Linq
+Imports System.Net.Http
+Imports System.Windows.Forms
+Imports System.Threading.Tasks
+Imports System.Diagnostics
+Imports Newtonsoft.Json
+
+Imports CmlLib.Core            ' MinecraftPath, CMLauncher
+Imports CmlLib.Core.Auth       ' MSession
+Imports CmlLib.Core.Version    ' MLaunchOption
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Public Class Form1
+    Inherits System.Windows.Forms.Form
+
+    ' --- CONFIGURATION ---
+    Private ReadOnly serverIp As String = "91.197.5.218"
+    Private ReadOnly serverPort As Integer = 26707
+    ' ----------------------
+
+    Public Sub New()
+        InitializeComponent()
+        Control.CheckForIllegalCrossThreadCalls = False
+        txtUsername.Text = "Pseudo"
+        txtUsername.ForeColor = Color.Gray
+    End Sub
+
+    Private Async Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Await PopulateVersionListAsync()
+    End Sub
+
+
+    Private Async Function PopulateVersionListAsync() As Task
+        cmbVersion.Items.Clear()
+        cmbVersion.Items.Add("Chargement...")
+        cmbVersion.SelectedIndex = 0
+
+        Try
+            Const manifestUrl As String = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
+            Using client As New HttpClient()
+                Dim json = Await client.GetStringAsync(manifestUrl)
+                Dim manifest = JsonConvert.DeserializeObject(Of VersionManifest)(json)
+                Dim releases = manifest.Versions _
+                    .Where(Function(v) v.Type = "release" AndAlso CompareVersionIds(v.Id, "1.8.8") >= 0) _
+                    .Select(Function(v) v.Id) _
+                    .ToList()
+                releases.Sort(AddressOf CompareVersionIds)
+
+                cmbVersion.Items.Clear()
+                For Each vid In releases
+                    cmbVersion.Items.Add(vid)
+                Next
+                cmbVersion.SelectedIndex = cmbVersion.Items.Count - 1
+            End Using
+
+        Catch ex As Exception
+            Debug.WriteLine($"[ERROR] chargement versions : {ex.Message}")
+            cmbVersion.Items.Clear()
+            cmbVersion.Items.Add("1.8.8")
+            cmbVersion.SelectedIndex = 0
+        End Try
+    End Function
+
+    Private Function CompareVersionIds(a As String, b As String) As Integer
+        Dim aSeg = a.Split("."c).Select(Function(s) If(Integer.TryParse(s, Nothing), Integer.Parse(s), 0)).ToArray()
+        Dim bSeg = b.Split("."c).Select(Function(s) If(Integer.TryParse(s, Nothing), Integer.Parse(s), 0)).ToArray()
+        Dim length = Math.Max(aSeg.Length, bSeg.Length)
+        For i = 0 To length - 1
+            Dim av = If(i < aSeg.Length, aSeg(i), 0)
+            Dim bv = If(i < bSeg.Length, bSeg(i), 0)
+            If av < bv Then Return -1
+            If av > bv Then Return 1
+        Next
+        Return 0
+    End Function
+
+    Private Function CreateOfflineSession(playerName As String) As MSession
+        Return New MSession() With {
+            .Username = playerName,
+            .UUID = playerName,
+            .AccessToken = "0",
+            .UserType = "legacy"
+        }
+    End Function
+
+    Private Async Sub LaunchMinecraftGame()
+        Dim versionToLaunch = cmbVersion.SelectedItem?.ToString()
+        If String.IsNullOrEmpty(versionToLaunch) Then
+            MessageBox.Show("Version invalide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            btnLaunch.Enabled = True
+            Return
+        End If
+
+        Dim path As New MinecraftPath(".Dindjulauncher")
+        Dim launcher As New CMLauncher(path)
+
+        Try
+            Dim launchOption As New MLaunchOption() With {
+                .MaximumRamMb = 2048,
+                .Session = CreateOfflineSession(txtUsername.Text),
+                .ServerIp = serverIp,
+                .ServerPort = serverPort
+            }
+
+            Debug.WriteLine($"[CMLIB] Lancement Minecraft {versionToLaunch}...")
+            Dim proc = Await launcher.CreateProcessAsync(versionToLaunch, launchOption)
+            proc.Start()
+
+            Hide()
+            Debug.WriteLine("[Launcher] Minecraft lancé.")
+        Catch ex As Exception
+            Debug.WriteLine($"[ERROR] {ex}")
+            MessageBox.Show($"Erreur de lancement :{Environment.NewLine}{ex.Message}", "Erreur Launcher", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            btnLaunch.Enabled = True
+        End Try
+    End Sub
+
+    Private Sub btnLaunch_Click(sender As Object, e As EventArgs) Handles btnLaunch.Click
+        If String.IsNullOrWhiteSpace(txtUsername.Text) OrElse txtUsername.Text = "Pseudo" Then
+            MessageBox.Show("Veuillez entrer un pseudo valide !", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+        btnLaunch.Enabled = False
+        Task.Run(AddressOf LaunchMinecraftGame)
+    End Sub
+
+    Private Sub txtUsername_Enter(sender As Object, e As EventArgs) Handles txtUsername.Enter
+        If txtUsername.Text = "Pseudo" Then
+            txtUsername.Text = ""
+            txtUsername.ForeColor = Color.Black
+        End If
+    End Sub
+
+    Private Sub txtUsername_Leave(sender As Object, e As EventArgs) Handles txtUsername.Leave
+        If String.IsNullOrWhiteSpace(txtUsername.Text) Then
+            txtUsername.Text = "Pseudo"
+            txtUsername.ForeColor = Color.Gray
+        End If
+    End Sub
+End Class
+
+' Toutes les classes déclarées après Form1 pour éviter les erreurs du Designer
+Public Class VersionManifest
+    Public Property Versions As List(Of VersionInfo)
+End Class
+
+Public Class VersionInfo
+    Public Property Id As String
+    Public Property Type As String
+    Public Property Url As String
+End Class
